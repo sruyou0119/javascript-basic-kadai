6aaf30be4d1f9b7831112e6d3b1331050c6b6ae1
saruyou0119 <ssyouhei1999011997@gmail.com>
2025-05-27 17:30:02 +0900

    Add kadai_014

--- JavaScript/js/function.js
+++ JavaScript/js/function.js
@@ -12,3 +12,25 @@ const sayGoodEvening = () => {
 sayGoodMorning();
 
 sayGoodEvening();
+
+
+const addTwoArguments = (price, shippingFee) => {
+    console.log(price + 500 + '円');
+}
+
+addTwoArguments(1200, 500);
+
+const double = (num) => {
+    return num * 2;
+}
+
+console.log(double(30));
+
+const useVariable = () => {
+    const userName = '侍太郎';
+    console.log(userName);
+}
+
+useVariable();
+
+console.log(userName);
